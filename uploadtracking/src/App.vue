a275c4befc3ec6a6d8a246a8687c5f3405f36e2b
etkabayramoglu <etkabo@gmail.com>
2019-01-27 13:39:18 -0600

    added missing <router-view> tag to the app.Vue

--- uploadtracking/src/App.vue
+++ uploadtracking/src/App.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
+    <router-view> </router-view>
     <button @click="openLoader()">BasBana</button>
 
     <LoadingSpinner v-if="showLoader"> </LoadingSpinner>
